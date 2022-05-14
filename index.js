d99b9c5d8abe9f685b0be02b16b5bcb472c09feb
danielshires <hello@danielshires.com>
2022-05-15 09:57:54 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+const main = document.querySelector("main#main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+document.appendChild(newHeader)
+
+
+
+
+// const newHEader = document.querySelector("h1#victory")
